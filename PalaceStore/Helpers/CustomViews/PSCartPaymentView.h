b48ad8c4a07395e4a633cb4fd37920e5306b4e2e
Sreelal H <sreelal.h@gmail.com>
2015-09-18 23:56:18 +0530

    Added new design

---
+++ PalaceStore/Helpers/CustomViews/PSCartPaymentView.h
@@ -0,0 +1,13 @@
+//
+//  PSCartPaymentview.h
+//  PalaceStore
+//
+//  Created by Sreelal H on 17/09/15.
+//  Copyright (c) 2015 Sreelal  Hamsavahanan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSCartPaymentView : UIView
+
+@end
